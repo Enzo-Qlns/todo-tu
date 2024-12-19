54451247138fa6d405783378bff6da9dee9392c2
Enzo QUELENIS <equelenis@myges.fr>
2024-12-19 11:16:51 +0100

    remove useless code

--- frontend/src/api/index.tsx
+++ frontend/src/api/index.tsx
@@ -5,24 +5,4 @@ const api = axios.create({
     baseURL: `${API_URL}/api/v1`,
 });
 
-// Add a request interceptor
-api.interceptors.request.use(
-    (config) => {
-        return config;
-    },
-    (error) => Promise.reject(error)
-);
-
-// Check status response
-api.interceptors.response.use(
-    (response) => {
-        if (response.status >= 200 && response.status < 300) {
-            return response;
-        } else {
-            return Promise.reject(response);
-        }
-    },
-    (error) => Promise.reject(error)
-);
-
-export default api
+export default api;
